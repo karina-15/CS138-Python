bc57626ff2ca7f90c1cc80238593471eba12779f
Karina Elias <karinaelias55@gmail.com>
2019-09-15 16:06:23 -0700

    Can draw rectangular base

---
+++ hw4/hw4extraCredit.py
@@ -0,0 +1,110 @@
+#! /usr/bin/python
+# Homework No.  04
+# Extra Credit
+# File Name:    hw4extraCredit.py
+# Programmer:   Karina Elias
+# Date:         Sep. 15, 2019
+#
+# Problem Statement: Programming Exercise #11 p.120
+# Five-click house
+# This program allows the user to draw a simple
+# house using 5 mouse-clicks. First 2 clicks draw
+# the opposite corners of the rectangular base. The
+# 3rd click will be the middle of the top edge of
+# the rectangular door. The door width is 1/5th the
+# width of the house frame. The 4th click is the
+# center of a square window which is 1/2 the width
+# of the door width. The last click is the peak of
+# the triangular roof.
+#
+#
+# Overall Plan:
+# 1. Create graphics window
+# 2. Print introduction
+# 3. Prompt user for 1st click (1 corner of base)
+# 4. Prompt user for 2nd click (2nd corner of base)
+# 5. Draw the rectangular base
+# 6. Prompt user for 3rd click (middle pt of door's
+#    top edge)
+# 7. Draw rectangular door 1/5th width of base
+# 8. Prompt user for 4th click (center of window)
+# 9. Draw square window 1/2 the door width
+# 10. Prompt user for last (5th) click (roof peak)
+# 11. Click to close window
+#
+#
+# import the necessary python libraries
+from graphics import *  # import everything defined in graphics.py
+
+
+def main():
+    # Create graphics window
+    win = GraphWin("Five-click house", 500, 500)
+    win.setBackground("#333333")  # Dark grey window background
+
+    # Print introduction
+    intro = Text(Point(250, 15), "This program allows you to draw a house")
+    intro.setTextColor("#FFFFFF")
+    intro.draw(win)
+
+    # ---Window Layout--- #
+
+    # Create white rectangle for house drawing area
+    house_canvas = Rectangle(Point(5, 30), Point(498, 470))
+    house_canvas.setFill("#FFFFFF")
+    house_canvas.draw(win)
+
+    # Display label at bottom of window for user prompt and information
+    info_label = Text(Point(250, 485), "Click in the white box to draw"
+                                       " the 1st corner of the house base.")
+    info_label.setTextColor("#FFFFFF")
+    info_label.draw(win)
+
+    # ---Input--- #
+    # Get user click point and draw the 1st corner
+    base_corner = win.getMouse()
+    base_x1 = base_corner.getX()
+    base_y1 = base_corner.getY()
+    base_corner_pt = Point(base_x1, base_y1)
+    base_corner_pt.draw(win)
+
+    # Update prompt at bottom of window
+    info_label.setText("Click again to draw the opposite corner"
+                       " of the house base")
+
+    # Get user click point for opposite corner of base
+    base_opp_corner = win.getMouse()
+    base_x2 = base_opp_corner.getX()
+    base_y2 = base_opp_corner.getY()
+    base_opp_corner_pt = Point(base_x2, base_y2)
+
+    # ---Process--- #
+    # Draw the rectangular base
+    rect_base = Rectangle(base_corner_pt, base_opp_corner_pt)
+    rect_base.setWidth(2)
+    rect_base.draw(win)
+
+    # Update prompt at bottom of window
+    info_label.setText("Click inside the house base to draw"
+                       " the top edge of the door")
+
+    # Get user click point for door's top edge
+    # and calculate door's rectangle opposite corner pts
+    door_top_middle_pt = win.getMouse()
+    base_width = base_x2 - base_x1
+    door_width = base_width/5
+
+
+
+
+
+
+    # ---Output--- #
+
+    # Close window
+    info_label.setText("Click to exit")
+    win.getMouse()  # pause for click in window
+    win.close()
+
+
+main()
