e7a033767bbd2d968bb84f80ed21948d832276a6
Karina Elias <karinaelias55@gmail.com>
2019-09-20 17:25:00 -0700

    Added hw5 directory

---
+++ hw5/hw5project1.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+# Homework No.  05
+# Project No.   01
+# File Name:    hw5project1.py
+# Programmer:   Karina Elias
+# Date:         Sep. 20, 2019
+#
+# Problem Statement:
+# 
+#
+#
+# Overall Plan:
+# 1. Create graphics window
+# 2. Print introduction
+# 3. Prompt user for 1 number using Entry box
+# 4. Prompt user for a 2nd number using Entry box
+# 5. Create a Calculate button with Rectangle method
+# 6. Display percentage w/ empty values
+# 7. Display sum w/ empty values
+# 8. Calculate percentage
+# 9. Calculate sum
+# 10. Change setText for percentage, sum, and button
+#     after window is clicked
+# 11. Click to close window
+#
+#
+# import the necessary python libraries
---
+++ hw5/textbookExamples.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python
+#
+# STRING OPERATIONS
+# concatenation +
+# repetition *
+# indexing <string>[]
+# slicing <string>[:]
+# length len(<string>)
+# iteration thru chars for <var> in <string>
+
+
+def main():
+    # ---pg.122---
+    # strings
+    str1 = "Hello"
+    str2 = 'spam'
+    print(str1, str2)
+    # Hello spam
+    type(str1)
+    # <class 'str'>
+    type(str2)
+    # <class 'str'>
+
+    # ---pg.123---
+    # string index
+    greet = "Hello Bob"
+    greet[0]
+    # 'H'
+    print(greet[0], greet[2], greet[4])
+    # H l o
+    x = 8
+    print(greet[x-2])
+    # B
+
+    # ---pg.124---
+    # negative indexing = last characters
+    greet[-1]
+    # 'b'
+    greet[-3]
+    # 'B'
+
+    # substrings/slicing
+    # <string>[<start-int>:<end-int-not-included>]
+    greet[0:3]
+    # 'Hel'
+    greet[5:9]
+    # ' Bob'
+    greet[:5]   # from start of string
+    # 'Hello'
+    greet[5:]   # til end of string
+    # ' Bob'
+    greet[:]    # entire string
+    # 'Hello Bob'
+
+    # ---pg.125---
+    "spam" + "eggs"
+    # 'spameggs'
+    "Spam" + "And" + "Eggs"
+    # 'SpamAndEggs'
+    3 * "spam"
+    # 'spamspamspam'
+    "spam" * 5
+    # 'spamspamspamspamspam'
+    (3 * "spam") + ("eggs" * 5)
+    # 'spamspamspameggseggseggseggseggs'
+    len("spam")
+    # 4
+    len("SpamAndEggs")
+    # 11
+    # for ch in "Spam!":
+    #     print(ch, end=" ")
+    # S p a m !
+
+
+main()
