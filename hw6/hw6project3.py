0cd5c0974b64d6c5cd147a8b9e7ee3286ff22e7a
Karina Elias <karinaelias55@gmail.com>
2019-09-30 21:58:04 -0700

    Add hw6project3 file

---
+++ hw6/hw6project3.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python
+# Homework No.  06
+# Project No.   03
+# File Name:    hw6project3.py
+# Programmer:   Karina Elias
+# Date:         Sep. 30, 2019
+#
+# Problem Statement: Programming Exercise #2 p.196
+# Write a program to print the lyrics for ten verses
+# of "The Ants Go Marching."
+#
+#
+# Overall Plan:
+# 1. Print introduction
+# 2. Prompt user to enter ...
+# 3. Prompt user to enter ...
+# 4. Calculate ...
+#    (use formula / logic / loop)
+# 5. Display the ...
+#
+#
+# import the necessary python libraries
+import math  # Makes the math library available for pi & sin
+
+
+def main():
+    print()
+    # Print introduction
+    print("This program will ...")
+    print("...")
+    print()
+
+    # ---Input--- #
+    # Get the ...
+    var1 = eval(input("Enter the ...: "))
+    # Get the ...
+    var2 = eval(input("Enter the ...: "))
+    print()
+
+    # ---Process--- #
+    # some calculation 
+    # var3 = var1 + var2
+    # some other calculation
+    # var4 = var3 * 10
+
+    # ---Output--- #
+    print("The ... var1 = %d var2 %.2f" % (var1, var2))
+
+
+main()
