ffb78a1ba19148b911615252b98f9996a9069972
Karina Elias <karinaelias55@gmail.com>
2019-11-28 23:40:39 -0800

    Update hw10project2.py completed, with comments and added screenshot

--- hw10/hw10project2.py
+++ hw10/hw10project2.py
@@ -14,48 +14,87 @@
 #
 #
 # Overall Plan:
-# 1.
+# 1. Create dictionary for suit
+# 2. Create graphics window include:
+#       Green background, banner text, instructions text,
+#       entry textbox, display suit button, image placeholder
+#       rectangle, quit button
+# 3. Get suit entry when display suit button is clicked
+# 4. Display image of corresponding suit
+# 5. Close window when quit button is clicked
 #
 #
 # import the necessary python libraries and classes
-# for this program graphics is used
+# for this program graphics and button is used
 from graphics import *
 from button import Button
 
-# ---FUNCTIONS---
+class GraphicsInterface:
+    # Create graphics window
+    def __init__(self):
+        # Dictionary for suit images
+        self.suit = dict(club='club.ppm',
+                         heart='heart.ppm',
+                         diamond='diamond.ppm',
+                         spade='spade.ppm')
+        self.win = GraphWin("Suit Display", 600, 400)
+        # Green background
+        self.win.setBackground("green")
+        # Banner text
+        banner = Text(Point(300, 30), "Display a Suit")
+        banner.setSize(24)
+        banner.setFill("yellow")
+        banner.setStyle("bold")
+        banner.draw(self.win)
+        # Instructions text
+        instructions = Text(Point(300, 60), "(e.g. club, heart, diamond, spade)")
+        instructions.setStyle("bold")
+        instructions.draw(self.win)
+        # Entry Textbox
+        self.entryTextBox = Entry(Point(300, 85), 10)
+        self.entryTextBox.draw(self.win)
+        # Display suit button
+        self.displaySuitButton = Button(self.win, Point(300, 115), 100, 20, "Display Suit")
+        self.displaySuitButton.activate()
+        # Image placeholder rectangle
+        self.imageDisplayRect = Rectangle(Point(225, 150), Point(375, 350))
+        self.imageDisplayRect.draw(self.win)
+        # Quit button
+        self.quitButton = Button(self.win, Point(300, 370), 100, 20, "Quit")
+        self.quitButton.activate()
+        # call function to display suit entered from user
+        self.displaySuit()
+
+    # ---FUNCTIONS---
+    # function to display suit entered from user
+    def displaySuit(self):
+        # get mouse click point
+        pt = self.win.getMouse()
+        # if "Quit" button is clicked, close window
+        while not self.quitButton.clicked(pt):
+            # if the "Display Suit" button is clicked and
+            # the entry textbox is not empty, display the
+            # suit image
+            if self.displaySuitButton.clicked(pt) \
+                    and self.entryTextBox.getText() is not '':
+                # image placeholder background turns white
+                self.imageDisplayRect.setFill('white')
+                inputText = self.entryTextBox.getText()
+                # display suit image in center of
+                # image placeholder rectangle
+                suitImage = Image(Point(300, 250), self.suit[inputText])
+                suitImage.draw(self.win)
+            # get mouse click point again to check if the "Quit"
+            # or "Display Suit" buttons were clicked
+            pt = self.win.getMouse()
+        # close window when "Quit" button is clicked
+        self.win.close()
+
 
 # ---MAIN---
 def main():
-    suit = {'club': 'club.ppm',
-            'heart': 'heart.ppm',
-            'diamond': 'diamond.ppm',
-            'spade': 'spade.ppm'}
-    win = GraphWin("Suit Display", 600, 400)
-    win.setBackground("green")
-    banner = Text(Point(300, 30), "Display a Suit")
-    banner.setSize(24)
-    banner.setFill("yellow")
-    banner.setStyle("bold")
-    banner.draw(win)
-    textBox = Entry(Point(300, 75), 10)
-    textBox.draw(win)
-    displayCardButton = Button(win, Point(300, 115), 100, 20, "Display Suit")
-    displayCardButton.activate()  # after something is entered in text box
-    rect = Rectangle(Point(225, 150), Point(375, 350))
-    rect.draw(win)
-    quitButton = Button(win, Point(300, 370), 100, 20, "Quit")
-    quitButton.activate()
-
-    pt = win.getMouse()
-    while not quitButton.clicked(pt):
-        if displayCardButton.clicked(pt)\
-                and textBox.getText() is not '':
-            rect.setFill('white')
-            inputText = textBox.getText()
-            card = Image(Point(300, 250), suit[inputText])
-            card.draw(win)
-        pt = win.getMouse()
-    win.close()
+    # Create GUI for suit display
+    GraphicsInterface()
 
 
 if __name__ == '__main__':
